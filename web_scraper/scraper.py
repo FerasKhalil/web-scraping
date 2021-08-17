825319293f4d4ab1a03535feb3ebc7cb60fee1ee
FIRAS AHMAD HASAN <x.firashasan@gmail.com>
2021-08-17 22:53:03 +0300

    added tests to 17 scraper

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -3,27 +3,30 @@ from bs4 import BeautifulSoup
 
 url = 'https://en.wikipedia.org/wiki/History_of_Mexico'
 
-def get_citations_needed_count(url) -> int:
+def get_citations_needed_count(url):
 
-    texts = requests.get(url).text
-    with open("file.html",'w') as file:
-        file.write(texts)    
+    texts = requests.get(url)
 
-    soup = BeautifulSoup(texts,'html.parser')
-    li_result = soup.findAll('li')
-    return len(li_result)
+    # with open("file.html",'w') as file:
+    #     file.write(texts)    
+
+    soup = BeautifulSoup(texts.content,'html.parser')
+    a_result = soup.findAll('a',title='Wikipedia:Citation needed')
+    return len(a_result)
 
 
-print(get_citations_needed_count(url))
+print (get_citations_needed_count(url))
 
 def get_citations_needed_report(url) -> str:
-    result_list=[]
+    result_list=''
     texts = requests.get(url).text
     soup = BeautifulSoup(texts,'html.parser')
     parags_result = soup.findAll('p')
+
     for i in parags_result:
-        if i.find(title = 'Americas'):
-            result_list.append(i)
+        if i.find('span'):
+            result_list+=i.text
+            result_list+='\n\n'
     return str(result_list)        
 print(get_citations_needed_report(url))
 
